49cd1301f75a0fdd933a489fffa083a57ddb5b7a
Andrey <andydeshko@gmail.com>
2022-03-09 23:26:27 +0200

    added filter

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+// const lineThrough = document.getElementsByClassName('completed');
 
 
 
@@ -56,9 +59,36 @@ const deleteCheck = (e) => {
     }
 }
 
+const filterTodo = function (e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                    // lineThrough.style.removeProperty('text-decoration');
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+
+        }
+    });
+}
 
 todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck)
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 
 
